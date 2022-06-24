651bbff8070cf2425be3d9480a3fcf497607c7b5
mcjovial <emmanuelelias455@gmail.com>
2022-06-24 16:52:40 +0100

    redux dev tool integration

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./Reducers";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+));
 
 export default store;
